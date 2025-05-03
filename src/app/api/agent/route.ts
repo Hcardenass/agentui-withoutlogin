6db236ffc63dac839d9435fa196f6fbefbde59d6
Adrian Cardenas <92567314+Hcardenass@users.noreply.github.com>
2025-05-02 20:12:02 -0500

    Update route.ts

--- src/app/api/agent/route.ts
+++ src/app/api/agent/route.ts
@@ -1,7 +1,7 @@
 import type { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
-  const url = '[api]?' +
+  const url = 'https://apifutbolista-845142198474.us-west4.run.app/equipo?futbolista=messi?' +
               new URL(request.url).searchParams.toString();
   const res = await fetch(url);
   const text = await res.text();
